5dc7b74556ed4214e993e7f9a9ee9e397ac2a64d
mphhohku <130424415+mphhohku@users.noreply.github.com>
2023-05-03 17:31:33 +0800

    2023 update

--- Azure/create-azure-ad-group.sh
+++ Azure/create-azure-ad-group.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 AZURE_AD_GROUP_NAME="devopsthehardway-aks-group"
-CURRENT_USER_OBJECTID=$(az ad signed-in-user show --query objectId -o tsv)
+CURRENT_USER_OBJECTID=$(az ad signed-in-user show --query id -o tsv)
 
 # Create Azure AD Group
 az ad group create --display-name $AZURE_AD_GROUP_NAME --mail-nickname $AZURE_AD_GROUP_NAME
 
-# Add Current az login user to Azure AD Group
+# Add Current az login user to Azure AD Group (2023)
 az ad group member add --group $AZURE_AD_GROUP_NAME --member-id $CURRENT_USER_OBJECTID
 
-AZURE_GROUP_ID=$(az ad group show --group "devopsthehardway-aks-group" --query objectId -o tsv)
+# 2023 update
+AZURE_GROUP_ID=$(az ad group show --group "devopsthehardway-aks-group" --query id -o tsv)
 
 echo "AZURE AD GROUP ID IS: $AZURE_GROUP_ID"
